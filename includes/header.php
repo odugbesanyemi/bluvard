aa8a05aa1c90f396984c09792d5ede1f4b8fa744
odugbesanyemi <odugbesanyemi@gmail.com>
2022-07-12 19:25:11 +0100

    Latest update

--- includes/header.php
+++ includes/header.php
@@ -6,46 +6,57 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Tecxha IT Solutions">
-    <meta name="description" content="Bluvard is a youth led organisation focused on bridging the education divide through non-conventional learning strategies for children in rural communities.">
+    <meta name="description"
+        content="Bluvard is a youth led organisation focused on bridging the education divide through non-conventional learning strategies for children in rural communities.">
     <title>Bluvard Education Initiative- Bridging the Education Divide</title>
-    <link rel="stylesheet" href="../css/style.css">
-    <link rel="stylesheet" href="../css/pages.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/pages.css">
     <link rel="stylesheet" href="slick-1.8.1/slick/slick-theme.css">
     <link rel="stylesheet" href="slick-1.8.1/slick/slick.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark heading">
-    <div class="container">
-        <a href="../index.php"><div class="navbar-brand d-flex align-items-center"><img src="../assets/bluvard2.svg" alt="" width="80">
-            <p class="ps-3 fw-bold text-white mb-0"><small> BLUVARD</small> </p>
-        </div></a>
-        <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a class="nav-link active mx-md-2" aria-current="page" href="../index.php">Home</a>
-                <a class="nav-link" href="../pages/about.php">About Us</a>
-                <a class="nav-link" href="../pages/event.php">Events</a>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Our Projects</a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="../pages/digital_rural.php">Digital Rural</a></li>
-                        <li><a class="dropdown-item" href="../pages/projects.php?girls_in_tech">Girls in Tech</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="../pages/events.php">New</a></li>
-                    </ul>
-                </li>
-                <a class="nav-link" href="../pages/gallery.php">Gallery</a>
-                <a class="nav-link" href="../pages/contact.php">contact Us</a>
-            </div>
-            <div id="nav-btn" class="ms-auto">
-                <a href="../pages/volunteer.php"><button type="button" class="btn btn-primary ms-3 d-flex justify-content-between align-items-center" id="volunteerBtn">Become a volunteer <ion-icon class="ps-2" name="help-buoy-outline"></ion-icon></button></a>
+    <nav class="navbar navbar-expand-lg navbar-dark  heading">
+        <div class="container">
+            <a href="../index.php">
+                <div class="navbar-brand d-flex align-items-center"><img src="../assets/bluvard2.svg" alt="" width="80">
+                    <p class="ps-3 fw-bold text-white mb-0"><small> BLUVARD</small> </p>
+                </div>
+            </a>
+            <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                    <a class="nav-link active mx-md-2" aria-current="page" href="../index.php">Home</a>
+                    <a class="nav-link" href="../pages/about.php">About Us</a>
+                    <a class="nav-link" href="../pages/event.php">Events</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">Our Projects</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="../pages/digital_rural.php">Digital Rural</a></li>
+                            <li><a class="dropdown-item" href="../pages/projects.php?girls_in_tech">Girls in Tech</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="../pages/events.php">New</a></li>
+                        </ul>
+                    </li>
+                    <a class="nav-link" href="../pages/gallery.php">Gallery</a>
+                    <a class="nav-link" href="../pages/contact.php">contact Us</a>
+                </div>
+                <div id="nav-btn" class="ms-auto">
+                    <a href="../pages/volunteer.php"><button type="button"
+                            class="btn btn-primary ms-3 d-flex justify-content-between align-items-center"
+                            id="volunteerBtn">Become a volunteer <ion-icon class="ps-2" name="help-buoy-outline">
+                            </ion-icon></button></a>
+                </div>
             </div>
         </div>
-    </div>
-</nav>
+    </nav>
